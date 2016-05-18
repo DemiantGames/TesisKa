b9f20f612fa6fef2330192ca9566233caa140a79
Demián <demiandal@gmail.com>
2016-05-18 17:59:12 -0500

    Imagenes 1-10 algunos errores al repetir la secuencia.

---
+++ Assets/scripts/Pregunta.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pregunta : MonoBehaviour {
+	private float tiempo;
+	// Use this for initialization
+	void Start () {
+		tiempo = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (tiempo <= 0) {
+			Destroy (this.gameObject);
+		}
+		tiempo -= Time.deltaTime;
+	}
+}
--- Assets/scripts/Spawner.cs
+++ Assets/scripts/Spawner.cs
@@ -10,8 +10,15 @@ public class Spawner : MonoBehaviour {
 	public controlBombon Bombon;
 	public GameObject BuenaPalabra;
 	public GameObject MalaPalabra;
+	public GameObject[] Pregunta= new GameObject[15];
+	public GameObject[] Respuesta= new GameObject[45];
+//	public GameObject Pregunta1;
+//	public GameObject Pregunta2;
+//	public GameObject Pregunta3;
+//	public GameObject Pregunta4;
+//	public GameObject Pregunta5;
 
-	private Vector3 ubication1,ubication2,ubication3;
+	private Vector3 ubication1, ubication2, ubication3, ubicationPregunta= new Vector3(5,0,0);
 	public bool AlienComunica = false;
 	// Use this for initialization
 	void Start () {
@@ -26,9 +33,9 @@ public class Spawner : MonoBehaviour {
 
 
 		completelevel = 0;
-		while(VectorPreguntas[NumPregunta]==1 && completelevel <=16){
+		while(VectorPreguntas[NumPregunta]==1 && completelevel <=15){
 			NumPregunta += 1;
-			if (NumPregunta > 15) {
+			if (NumPregunta > 14) {
 				NumPregunta = 0;
 			}
 
@@ -37,7 +44,7 @@ public class Spawner : MonoBehaviour {
 		}
 
 
-		if (tiempo <= 0 && completelevel<=16) {
+		if (tiempo <= 0 && completelevel<=15) {
 
 			Spawn ();
 			tiempo=TimeSpawn;
@@ -90,85 +97,95 @@ public class Spawner : MonoBehaviour {
 		if (NumPregunta <= CantiPreguntas) {
 			
 			if (NumPregunta == 0) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[0], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[0], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[1], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[2], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 1) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[1], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[3], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[4], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[5], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 2) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[2], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[6], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[7], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[8], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 3) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[3], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[9], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[10], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[11], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 4) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[4], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[12], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[13], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[14], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 5) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[5], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[15], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[16], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[17], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 6) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[6], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[18], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[19], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[20], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 7) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[7], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[21], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[22], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[23], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 8) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[8], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[24], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[25], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[26], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 9) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[9], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[27], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[28], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[29], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 10) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[10], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[30], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[31], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[32], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 11) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[11], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[33], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[34], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[35], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 12) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[12], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[36], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[37], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[38], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 13) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[13], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[39], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[40], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[41], ubication3, Quaternion.identity);
+
 			} else if (NumPregunta == 14) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
-			} else if (NumPregunta == 15) {
-				Instantiate (BuenaPalabra, ubication1, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication2, Quaternion.identity);
-				Instantiate (MalaPalabra, ubication3, Quaternion.identity);
-				//Instanceo Instantiate (Pregunta, new Vector3(0,0,0), Quaternion.identity);
+				Instantiate (Pregunta[14], ubicationPregunta, Quaternion.identity);
+				Instantiate (Respuesta[42], ubication1, Quaternion.identity);
+				Instantiate (Respuesta[43], ubication2, Quaternion.identity);
+				Instantiate (Respuesta[44], ubication3, Quaternion.identity);
+
 			} 
 
 			AlienComunica = true;
@@ -176,7 +193,7 @@ public class Spawner : MonoBehaviour {
 		np = NumPregunta;
 		NumPregunta += 1;
 
-		if (NumPregunta > 15) {
+		if (NumPregunta > 14) {
 			NumPregunta = 0;
 		}
 
